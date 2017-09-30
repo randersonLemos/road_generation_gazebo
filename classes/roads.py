7989d0510e8fab08078bab4de6547f06febd18b9
Randerson Lemos <randerson.lemos@gmail.com>
2017-09-29 21:31:33 -0300

    Initial implementation of class Road

--- classes/roads.py
+++ classes/roads.py
@@ -4,7 +4,6 @@ from frozen import FrozenClass
 from scipy import interpolate
 
 
-
 class Road(FrozenClass):
   """
   Computes a circuit (road) from a list of points.
@@ -21,14 +20,78 @@ class Road(FrozenClass):
   def __init__(self, pts, close=1, degree=3):
     [self.x, self.y] = zip(*pts)
     self.tck, self.t = interpolate.splprep([self.x, self.y], s=0, per=close, k=degree)
+    self._figure_on = False
 
 
-  def show_road(self):
-    import matplotlib.pyplot as plt
-    t = np.arange(0.0,1.01,0.01)
+  def road(self, t):
     out = interpolate.splev(t, self.tck)
-    plt.figure
-    plt.plot(self.x, self.y, 'o')
-    plt.plot(out[0],out[1])
-    plt.show()
+    return out
 
+
+  def der_road(self, t, order=1):
+    out = interpolate.splev(t, self.tck, der=order)
+    return out
+
+
+  def offset_road(self, offset, npts=100):
+    self.size = npts
+    t = np.arange(self.t[0], self.t[-1]+1.0/npts, 1.0/npts)
+    [x,y] = self.road(t)
+    [dx,dy] = self.der_road(t)
+
+    self.xr = x + offset*dy/np.sqrt(dx**2 + dy**2)
+    self.yr = y - offset*dx/np.sqrt(dx**2 + dy**2)
+
+    self.xl = x - offset*dy/np.sqrt(dx**2 + dy**2)
+    self.yl = y + offset*dx/np.sqrt(dx**2 + dy**2)
+
+
+  def noise_offset_road(self, mean=0, var=1):
+    self.xrn = self.xr + np.random.normal(mean, var, self.size+1)
+    self.yrn = self.yr + np.random.normal(mean, var, self.size+1)
+
+    self.xln = self.xl + np.random.normal(mean, var, self.size+1)
+    self.yln = self.yl + np.random.normal(mean, var, self.size+1)
+
+
+  def plot_offset_road(self):
+    self._plot_road()
+    self._plot_offset_road()
+    self._plt.axis('equal')
+    self._plt.show()
+
+
+  def plot_noise_offset_road(self):
+    self._plot_road()
+    self._plot_noise_offset_road()
+    self._plt.axis('equal')
+    self._plt.show()
+
+
+  def _load_figure_object(self):
+    import matplotlib.pyplot as plt
+    self._plt = plt
+    self._figure_on = True
+
+
+  def _plot_road(self):
+    if not self._figure_on:
+      self._load_figure_object()
+    t = np.arange(self.t[0], self.t[-1]+0.01, 0.01)
+    [x, y] = self.road(t)
+    self._plt.plot(self.x, self.y, 'o')
+    self._plt.plot(x ,y)
+
+
+  def _plot_offset_road(self):
+    if not self._figure_on:
+      self._load_figure_object()
+    self._plt.plot(self.xl, self.yl, 'o')
+    self._plt.plot(self.xr, self.yr, 'o')
+
+
+  def _plot_noise_offset_road(self):
+    if not self._figure_on:
+      self._load_figure_object()
+    self._plt.plot(self.xln, self.yln, 'o')
+    self._plt.plot(self.xrn, self.yrn, 'o')
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import numpy as np
 import classes.roads as roads
 
-pts = [(-2,-2),(0,-2),(2,-2),(2,0),(2,2),(0,2),(-2,2),(-2,0),(-2,-2)]
-rd = roads.Road(pts)
-rd.show_road()
+x = np.arange(0, 2*np.pi+np.pi/4, 2*np.pi/8)
+y = np.sin(x)
+
+#pts = [(-2,-2),(0,-2),(2,-2),(2,0),(2,2),(0,2),(-2,2),(-2,0),(-2,-2)]
+pts = zip(x,y)
+
+rd = roads.Road(pts, close=0)
+rd.offset_road(0.50, npts=100)
+rd.noise_offset_road(var=0.10)
+#rd.plot_offset_road()
+#rd.plot_noise_offset_road()
