56dfa4112e8c21aa64f788cbb8663d5a1f5b0516
Lucas Nogueira <lukscasanova@gmail.com>
2017-12-12 17:43:03 -0200

    Minor changes...

--- bin/road_gazebo.py
+++ bin/road_gazebo.py
@@ -12,61 +12,67 @@ def main(road_pts, road_close, offset
         ,offset_npts, offset_var, doplot
         ,model, file_name):
 
-  rd = roads.Road(pts=road_pts
-                 ,close=road_close
-  )
+    rd = roads.Road(
+             pts=road_pts
+            ,close=road_close
+            )
 
-  rd.offset_road(offset=offset
-                ,npts=offset_npts
-  )
+    rd.offset_road(
+             offset=offset
+            ,npts=offset_npts
+            )
 
-  rd.noise_offset_road(var=offset_var
-  )
+    rd.noise_offset_road(
+            var=offset_var
+            )
 
-  poses=utils.position_to_pose(rd.get_noise_offset_road_points()
-  )
+    poses=utils.position_to_pose(
+            rd.get_noise_offset_road_points()
+            )
 
-  wds = worlds.World(model=model
-                    ,poses=poses
-                    ,file_name = file_name
-  )
+    wds = worlds.World(
+             model=model
+            ,poses=poses
+            ,file_name = file_name
+            )
 
-  wds.make_custom_world(
-  )
+    wds.make_custom_world()
 
-  if doplot:
-    rd.plot_road()
-    rd.plot_offset_road()
-    if offset_var:
+    if doplot:
+        rd.plot_road()
+        rd.plot_offset_road()
         rd.plot_noise_offset_road()
-    rd.plot_show()
-
+        rd.plot_show()
 
 if __name__ == '__main__':
 
-  # Initialize the node and name it.
-  rospy.init_node('world_creation', anonymous=True, disable_signals=True)
+    # Initialize the node and name it.
+    rospy.init_node('world_creation', anonymous=True, disable_signals=True)
 
-  # Getting parameters
-  metadata = rospy.get_param('~metadata')
-  road_close=metadata['close']
-  if metadata['mode'] == 'function':
-    if metadata['geometry'] == 'ellipse':
-      road_pts = generates.ellipse(metadata['radius']
-                                  ,metadata['RADIUS']
-      )
-  elif metadata['mode'] == 'waypoints':
-      road_pts = metadata['points']
-  else:
-    raise ValueError('Not expected mode...')
+    # Getting parameters
+    metadata = rospy.get_param('~metadata')
+    road_close=metadata['close']
+    if metadata['mode'] == 'function':
+        if metadata['geometry'] == 'ellipse':
+            road_pts = generates.ellipse(
+                    metadata['radius']
+                   ,metadata['RADIUS']
+                   )
+    elif metadata['mode'] == 'waypoints':
+        road_pts = metadata['points']
+    else:
+        raise ValueError('Not expected mode...')
 
-  main(road_pts=road_pts
-      ,road_close=road_close
-      ,offset=rospy.get_param('~offset')
-      ,offset_npts=rospy.get_param('~offset_npts')
-      ,offset_var=rospy.get_param('~offset_var')
-      ,doplot=rospy.get_param('~doplot')
-      ,model=rospy.get_param('~model')
-      ,file_name =rospy.get_param('~file_name')
-  )
+    main(
+             road_pts=road_pts
+            ,road_close=road_close
+            ,offset=rospy.get_param('~offset')
+            ,offset_npts=rospy.get_param('~offset_npts')
+            ,offset_var=rospy.get_param('~offset_var')
+            ,doplot=rospy.get_param('~doplot')
+            ,model=rospy.get_param('~model')
+            ,file_name =rospy.get_param('~file_name')
+            )
+
+    print '.world file generated and saved at world folder as ' + rospy.get_param('~file_name') + '...'
 
