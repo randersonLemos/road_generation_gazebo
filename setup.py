112b2d76e390d08a69d7232664a00a7fea3a704d
Randerson Lemos <ranarelemos@msn.com>
2018-11-28 12:16:08 -0200

    Renamed folder bin to script and performed necessary fixes

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
   packages=['road_generation_gazebo'],
-  scripts=['bin/road_gazebo.py'],
-  package_dir={'': 'src'},
+  scripts=['script/road_gazebo.py'],
+  package_dir={'':'src'},
 )
 
 setup(**setup_args)
